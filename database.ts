3a06a9c1be194864486e7de552f59357a598b1e4
Freddy-Rios <freddyrios2000@gmail.com>
2023-05-06 17:36:43 -0700

    comments

--- database.ts
+++ database.ts
@@ -1,2 +1,5 @@
 // this is for all database access
 // what database are we going to be using
+
+// for the database we will need the bcrypt and uuid
+// these will be for the tokens and to hash the passcode
