c95c94505a0411feeb83914e195b1ab869d8ca10
Freddy-Rios <freddyrios2000@gmail.com>
2023-07-11 20:27:59 -0600

    removed files

--- index.ts
+++
@@ -1,3 +0,0 @@
-// here we will have all are apis(internal IDK about third party)
-// grab packages like express, bycrypt, and cookieParser
-// express will allow us to make post and get req to the database
