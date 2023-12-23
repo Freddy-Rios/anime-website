377a1098fd910e8295001b47d81bf11c3b12ddd4
Freddy-Rios <freddyrios2000@gmail.com>
2023-12-23 14:41:02 -0700

    added test assests and home banner

--- anime-website/src/app/page.js
+++ anime-website/src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Home from "@/views/Home";
 
-export default function Home() {
+export default function Homepage() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
--- anime-website/src/views/Home.js
+++ anime-website/src/views/Home.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import React from "react";
+import Image from "next/image"; // Import Image from Next.js
 
-const Navbar = () => {
+// Import your local image
+import banner from "@/assets/art/newbanner.png";
+
+const Home = () => {
   return (
     <>
-      <image />
+      <div style={{ height: "440px", width: "100%" }}>
+        <div style={{ height: "440px", width: "100%", position: "relative" }}>
+          <Image
+            src={banner}
+            alt="Placeholder"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+      <h1>hello</h1>
     </>
   );
 };
+
+export default Home;
