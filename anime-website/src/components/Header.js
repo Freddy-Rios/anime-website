65bd6689d1792ac760327cc010c98c7d89c1f85c
Freddy-Rios <freddyrios2000@gmail.com>
2024-01-01 16:54:12 -0700

    className for header component

--- anime-website/src/components/Header.js
+++ anime-website/src/components/Header.js
@@ -4,15 +4,22 @@ import { ThemeToggler } from "./ThemeToggler";
 
 function Header() {
   return (
-    <header>
-      <Link href="/">My Navbar</Link>
+    // Header found on all pages of website
+    <header
+      className="fixed w-full z-20 top-0 flex items-center 
+                justify-between p-5 bg-gradient-to-t 
+                from-gray-200/0 via-gray-900/25 to-gray-900"
+    >
+      <Link href="/" className="mr-10">
+        <h1>MY Navbar</h1>
+      </Link>
 
       {/* left area where the search and genre options will be  */}
       <div className="flex space-x-2">
         <h1>placeholder</h1>
         {/* genre dropdown */}
         {/* search input */}
-        {/* optional theme toggler */}
+        {/* dark and light mode toggler */}
         <ThemeToggler />
       </div>
     </header>
