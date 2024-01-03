0f358a6a3d95ce915d19a7244cea02fd0a845805
Freddy-Rios <freddyrios2000@gmail.com>
2024-01-03 11:22:39 -0700

    comments

--- anime-website/src/components/SearchInput.js
+++ anime-website/src/components/SearchInput.js
@@ -8,13 +8,16 @@ import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 
+// this will limit the user to a search between 2 - 50 characters
 const formSchema = z.object({
   input: z.string().min(2).max(50),
 });
 
 function SearchInput() {
+  // router is used to route searches to the search page
   const router = useRouter();
 
+  //Define the form
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -23,11 +26,18 @@ function SearchInput() {
   });
 
   function onSubmit(values) {
+    //Testing reasons
     console.log(values);
+
+    //to change url link
     router.push(`/search/${values.input}`);
+
+    //clear user input on search bar
+    form.reset();
   }
 
   return (
+    // taken from shadcn following and tweeking there example
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
